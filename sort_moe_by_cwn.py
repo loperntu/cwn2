42a02e36d6ab36fe9f95c3f7c04e2a4b75c10b1e
mhshih <d00142002@ntu.edu.tw>
2015-02-13 23:26:19 +0800

    sort moe by age

--- sort_moe_by_cwn.py
+++ sort_moe_by_cwn.py
@@ -14,29 +14,14 @@ for line in open('dict-revised.csv').readlines():
     if title not in moe_dict:moe_dict[title]=[definition]
     else:moe_dict[title].append(definition)
 
-for word in cwn_dict:
-    if word in moe_dict:
-        sense_list=cwn_dict[word]
-
-        print 'cwn:'
-        for i in range(len(sense_list)):
-            print sense_list[i]
-            pass
-
-        print 'moe:'
+for word in moe_dict:
+    if word in cwn_dict:
+        sense_rank={}
         for moe_sense in moe_dict[word]:
-            print moe_sense
-
-            i=flag=0
-            while i<len(sense_list):
-       #     for i in range(len(sense_list)):
-                if distance(sense_list[i],moe_sense)<30:
-                    if flag:sense_list.pop(flag)
-                    else:flag=i
-                i+=1
-
-        print 'merged:'
-        for sense in sense_list:
-            print word,',',sense
-
-        print
+            max=0
+            for cwn_sense in cwn_dict[word]:
+                d=distance(moe_sense,cwn_sense)
+                if d>max:max=d
+            sense_rank[moe_sense]=max
+        for sense,rank in sorted(sense_rank.items(),key=lambda x:x[1],reverse=True):
+            print word,sense
