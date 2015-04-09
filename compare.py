46337e08da77841b2c8b7bc4c4af5bfe52b2a018
mhshih <d00142002@ntu.edu.tw>
2015-04-09 16:41:00 +0800

    print different Janice,Iju,POS,ID,Lemma,Gloss,Example_800.txt

--- compare.py
+++ compare.py
@@ -1,5 +1,34 @@
-Iju800=open('Iju800.txt').readlines()
-Janice800=open('Janice800.txt').readlines()
-for i in range(len(Iju800)):
-    if Janice800[i]!=Iju800[i]:print Janice800[i].strip(),Iju800[i].strip()
+janice_supersense={
+'3':'noun.Tops',
+'4':'noun.act',
+'5':'noun.animal',
+'6':'noun.artifact',
+'7':'noun.attribute',
+'8':'noun.body',
+'9':'noun.cognition',
+'10':'noun.communication',
+'11':'noun.event',
+'12':'noun.feeling',
+'13':'noun.food',
+'14':'noun.group',
+'15':'noun.location',
+'16':'noun.motive',
+'17':'noun.object',
+'18':'noun.person',
+'19':'noun.phenomenon',
+'20':'noun.plant',
+'21':'noun.possession',
+'22':'noun.process',
+'23':'noun.quantity',
+'24':'noun.relation',
+'25':'noun.shape',
+'26':'noun.state',
+'27':'noun.substance',
+'28':'noun.time'
+}
+
+for line in open('goodSynset_Nabcd_ID_Lemma_Gloss_Example_800.tsv'):
+    janice,iju,pos,id,lemma,gloss,example=line.split()
+    janice=janice_supersense[janice]
+    if janice!=iju:print janice,iju,pos,id,lemma,gloss,example
     else:print
