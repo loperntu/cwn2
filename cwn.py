eea468c0be5b22c32f179ea5619222042c1c533b
mhshih <d00142002@ntu.edu.tw>
2015-12-03 20:13:16 +0800

    cwn2.Synset(all_lemma_names[0].1)

--- cwn.py
+++ cwn.py
@@ -1,23 +1,24 @@
-
 d=dict()
 
 for line in open('cwn_dirty.uniq'):
     lemma_name,definition=line.split()
-    if lemma_name not in d:d[lemma_name]=set([definition])
-    else:d[lemma_name].add(definition)
+    if lemma_name not in d:d[lemma_name]=[definition]
+    else:d[lemma_name].append(definition)
 
-all_lemma_names=set()
-for lemma_name,definitions in d.items():
-    all_lemma_names.add(lemma_name)
-
-def definitions(lemma_name):
-    return d[lemma_name]
+all_lemma_names=[lemma_name for lemma_name in d]
 
 class Synset:
     def __init__(self,lemma_name_index):
-        lemma_name,index=lemma_names_index.split('.')
-        self.definitions=['animal','canine']
+        lemma_name,index=lemma_name_index.split('.')
+        self.lemma_names=[lemma_name]
+        self.definition=d[lemma_name][int(index)]
+
+def synset(lemma_name_index):
+    return Synset(lemma_name_index)
 
 def synsets(lemma_name):
     synset_list=[]
-    for definition in d[lemma_name]
+    for i in range(len(d[lemma_name])):
+        synset_list+=[synset('%s.%d' % (lemma_name,i))]
+    return synset_list
+
